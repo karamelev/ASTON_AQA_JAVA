ca472d0a68e83d81448e8bbabc53d2f2f010dfd9
karamelev <karamelev@tut.by>
2023-10-16 21:01:31 +0300

    Initial commit

---
+++ src/main/java/Lesson_1/Main.java
@@ -0,0 +1,19 @@
+package Lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintWord printWord = new PrintWord();
+        printWord.printThreeWord();
+
+        CheckSumSign checkSumSign = new CheckSumSign();
+        checkSumSign.checkSumSign();
+
+        PrintColor printColor = new PrintColor();
+        printColor.printColor();
+
+        CompareNumbers compareNumbers = new CompareNumbers();
+        compareNumbers.compareNumbers();
+
+    }
+
+}
---
+++ src/main/java/Lesson_1/PrintColor.java
@@ -0,0 +1,20 @@
+package Lesson_1;
+
+import java.util.Scanner;
+
+public class PrintColor {
+    public void printColor () {
+
+        System.out.println("ВВедите value: ");
+        Scanner in  = new Scanner(System.in);
+        int value = in.nextInt();
+        if (value <= 0) {
+            System.out.println("Красный");
+            return;
+        } if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
